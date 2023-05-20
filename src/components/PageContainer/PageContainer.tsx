1cdc94e24f8013b9f4ce7f8ea3b476ee9b6e1d83
noone <kolosochek@gmail.com>
2023-05-20 13:41:06 +0200

    netlify fix

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,5 +1,5 @@
 import {Content, Footer, Header} from "antd/es/layout/layout";
-import {ConfigProvider, Layout} from "antd";
+import {Layout} from "antd";
 import {Outlet} from "react-router-dom";
 import "antd/dist/reset.css"
 
